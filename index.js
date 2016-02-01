d3af20721aa98309592d1f0647047129dde122d5
kpie89 <kpie89@gmail.com>
2016-02-01 06:36:30 -0500

    more detailed css and initial JS functionality

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+    $(document).ready(function(){
+        var turnCount=0;
+
+          $('#board').find('td').on('click', function(){
+                if (turnCount % 2 === 0){
+                  $(this).text('X');
+
+                } else {
+               //player 2's turn (O)
+                  $(this).text('O');
+
+                }
+              turnCount++;
+    });
+    $('#newGame').on('click',function(){
+        $('.square').text('');
+    });
+});
+
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
