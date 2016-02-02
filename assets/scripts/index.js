6777c44c02116bbef3231c2a79f4e52e79760dce
kpie89 <kpie89@gmail.com>
2016-02-02 16:15:59 -0500

    logic update: cats game and push id value into board

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,44 @@
 'use strict';
+// let playGame = function() {
+// let currentPlayer = 'X';
+//let squares = $('.sq');
+let turnCount = 0;
+ // let xWins = 0;
+ // let oWins = 0;
+const board = ['','','','','','','','',''];
+let winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
+let getWinner = function() {
+  if (winning[0][0] === ['X', 'X', 'X']) {
+    console.log(winning);
+    window.alert('winner is x');
+  return 'winner is x';
+}
+};
+$(document).ready(function () {
+      $('#board').find('td').click(function (){
+        if (turnCount % 2 === 0){
+          $(this).text('X');
+          board[event.target.id] = 'X';
+            } else {
+          $(this).text('O');
+          board[event.target.id] = 'O';
+    }
+    getWinner();
+    console.log(board);
+          turnCount++;
+          if (turnCount >= 9) {
+            window.alert("Cats game. Please play again.");
+          }
+    });
+});
+
+
+
+
+$('#playAgain').on('click', function(){
+  $('.sq').text('');
+});
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-'use strict';
-
-    $(document).ready(function(){
-        let turnCount=0;
-
-          $('#board').find('td').on('click', function(){
-                if (turnCount % 2 === 0){
-                  $(this).text('X');
-                } else {
-                  $(this).text('O');
-                }
-              turnCount++;
-    });
-
-    $('#playAgain').on('click',function(){
-        $('.square').text('');
-    });
-});
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
