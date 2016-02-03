643ee9df1dcddc75ec3955916a37534725336deb
kpie89 <kpie89@gmail.com>
2016-02-03 10:45:20 -0500

    functioning game logic!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,11 @@
 'use strict';
 // let playGame = function() {
-// let currentPlayer = 'X';
+let currentPlayer = 'X';
 //let squares = $('.sq');
 let turnCount = 0;
 // let xWins = 0;
 // let oWins = 0;
-const board = ['', '', '', '', '', '', '', '', ''];
+let board = ['', '', '', '', '', '', '', '', ''];
 let winning = [
   [0, 1, 2],
   [3, 4, 5],
@@ -17,46 +17,73 @@ let winning = [
   [2, 4, 6]
 ];
 
-let getWinner = function() {
-   for (let i = 0; i < winning.length; i++) {
-       let p1 = winning[i][0]; //|| winning[i][3]; //|| winning[i][6] || winning[i][0] || winning[i][1] || winning[i][2] || winning[i][0] || winning[i][2];
-       let p2 = winning[i][1]; //|| winning[i][4]; //|| winning[i][7] || winning[i][3] || winning[i][4] || winning[i][5] || winning[i][4] || winning[i][4];
-       let p3 = winning[i][2]; //|| winning[i][5]; //|| winning[i][8] || winning[i][6] || winning[i][7] || winning[i][8] || winning[i][8] || winning[i][6];
-       if ( (board[p1] === 'X') && ( board[p2] === 'X') && ( board[p3] === 'X') ){
-          console.log('winner is X');
-       } else if ( (board[p1] === 'O') && ( board[p2] === 'O') && ( board[p3] === 'O') ) {
-          console.log('winner is O');
-       } break;
+let resetBoard = function(){
+   $('#board').find('td').text('');
+   turnCount = 0;
+   board = ['','','','','','','','',''];
+ };
 
-  //   for (let j = 0; i < winning[i].length; j++) {
-  //  let winner = winning[i][j];
-  //    debugger;
-  //    if (winner[j] === 'X') {
-  //      debugger;
-  //    console.log('winner');
-  //   return 'winner';
-  //    }
-  //  }
-   }
-  //console.log('winner');
-  //return;
+let changePlayer = function(){
+   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+ };
+
+let getWinner = function(player) {
+  console.log(board);
+for (let i = 0; i < winning.length; i++) {
+  //console.log(winning.length);
+  //console.log(winning[i]);
+  let newArray = winning[i];
+  if (board[newArray[0]] === player && board[newArray[1]] === player && board[newArray[2]] === player){
+    console.log(player + 'wins');
+    window.alert(player + ' wins!');
+    resetBoard();
+  }
+//  for (let j = 0; j < newArray.length; j++) {
+//    debugger;
+//    console.log(newArray.length);
+//    console.log(newArray[j]);
+//    debugger;
+    //  console.log('winner is x');
+  }
+// } return;
 };
+//let p1 = winning[i][0]; //  || winning[1][3] || winning[2][6] || winning[3][0] || winning[4][1] || winning[5][2] || winning[6][0] || winning[7][2];
+//let p2 = winning[i][1]; //  || winning[1][4] || winning[2][7] || winning[3][3] || winning[4][4] || winning[5][5] || winning[6][4] || winning[7][4];
+//let p3 = winning[i][2]; //  || winning[1][5] || winning[2][8] || winning[3][6] || winning[4][7] || winning[5][8] || winning[6][8] || winning[7][6];
+//if ( (board[p1] === 'X') && ( board[p2] === 'X') && ( board[p3] === 'X') ){
+//    console.log('winner is X');
+//} else if ( (board[p1] === 'O') && ( board[p2] === 'O') && ( board[p3] === 'O') ) {
+//    console.log('winner is O');
+// } break;
+
+//   for (let j = 0; i < winning[i].length; j++) {
+//  let winner = winning[i][j];
+//    debugger;
+//    if (winner[j] === 'X') {
+//      debugger;
+//    console.log('winner');
+//   return 'winner';
+//    }
+//  }
+// }
+//console.log('winner');
+//return;
+//};
 
 
 $(document).ready(function() {
   $('#board').find('td').click(function() {
-    if (turnCount % 2 === 0) {
-      $(this).text('X');
-      board[event.target.id] = 'X';
-    } else {
-      $(this).text('O');
-      board[event.target.id] = 'O';
-    }
-    console.log(board);
+      $(this).text(currentPlayer);
+      board[event.target.id] = currentPlayer;
+      $(this).text(currentPlayer);
+      board[event.target.id] = currentPlayer;
+    // console.log(board);
     turnCount++;
-    getWinner();
+    getWinner(currentPlayer);
+    changePlayer();
     if (turnCount >= 9) {
       window.alert("Cats game. Please play again.");
+      resetBoard();
     }
   });
 });
